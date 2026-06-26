0613debe9740632d20b07db80265e5654bb0056f
MGRishard <marcin.jakub.gorny@gmail.com>
2026-06-26 16:15:24 +0200

    1st commit

---
+++ Week_1/Hello_Report.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+with open("report.md", "w", encoding="utf-8") as file:
+    file.write("# My first SRE report\n\nStatus: OK\n")
+print("Raport wygenerowany pomyślnie!")
---
+++ Week_1/report.md
@@ -0,0 +1,3 @@
+# My first SRE report
+
+Status: OK
